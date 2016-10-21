212c6190e7b7102ec9aa32fe9f30fc3ecf763804
Marx Medeiros <analista.marx@gmail.com>
2016-10-21 10:59:54 -0200

    Criado Modal

---
+++ almox/modalAlmox.php
@@ -0,0 +1,17 @@
+<!-- MODAL DE EXEMPLO -->
+<div id="novoLaudo" class="modal fade" role="dialog">
+ <div class="modal-dialog">
+  <div class="modal-content">
+   <div class="modal-header bg-red">
+    <button type="button" class="close" data-dismiss="modal">X</button>
+     <h4 class="modal-title">Adicionar Pedido de Laudo</h4>
+   </div>
+   <div class="modal-body">
+    
+
+   </div>
+   <div class="modal-footer"></div>
+  </div>
+ </div>
+</div>
+<!-- MODAL DE EXEMPLO -->
