3a9a68ca630017c09acca8590fb819ab446c47ec
Marx Medeiros <analista.marx@gmail.com>
2016-10-24 15:05:28 -0200

    revisado

--- QueryUser.php
+++ QueryUser.php
@@ -3,5 +3,13 @@
  $query->execute();
   $row = $query->fetch();
   $NomeUserLogado = $row['Nome'];
+  $MetaPrevista = $row['MetaPrevista']; 		// META PREVISTA DO USUÁRIO
+  $MetaRealizada = $row['MetaRealizada']; 		// META ALCANÇADA
+  $NomeUserLogado = $row['Nome'];				// NOME DO USUÁRIO
+  $PermMontagem = $row['P2Montagem'];			// PERMISSÃO PARA MONTAR RELOGIO
+  $PermReteste = $row['P2Reteste'];				// PERMISSÃO PARA RETESTAR RELÓGIO
+  $PermAdm = $row['P2Adm'];						// PERMISSÃO PARA ADMINISTRAR USUÁRIOS
+  $Catraca = $row['CadCat'];					// PERMISSÃO PARA ADICIONAR CATRACAS
+  $Plaudo = $row['PrivLaudo'];					// PERMISSÃO PARA ADICIONAR LAUDOS
 
 ?>
