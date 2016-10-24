a013a07554dc56fda8d113a2476fe28491e003be
Marx Medeiros <analista.marx@gmail.com>
2016-10-24 15:25:24 -0200

    V1

---
+++ menuLateral.php
@@ -0,0 +1,14 @@
+<ul class="sidebar-menu">
+ <li class="header"></li>
+  <li class="<?php echo $cHome; ?>">
+   <a href="<?php echo $server; ?>/dashboard.php">
+    <i class="fa fa-home"></i> <span>Início</span>
+   </a>
+  </li>
+  <li class="treeview <?php echo $cLogistica; ?>">
+   <a href="#">
+    <i class="fa fa-pie-chart"></i><span>Logística</span>
+    <span class="pull-right-container">
+     <i class="fa fa-angle-left pull-right"></i>
+    </span>
+   </a>
