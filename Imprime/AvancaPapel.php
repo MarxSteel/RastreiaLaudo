ebf6e9ea3ec4a24b68aa74e9a4d6c04b20e399da
Marx Medeiros <analista.marx@gmail.com>
2016-10-26 08:14:51 -0200

    Revisao

--- Imprime/AvancaPapel.php
+++ Imprime/AvancaPapel.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../init.php';
- $Imprime = "C[5[AVANCANDO PAPEL";          
+ $Imprime = "C[1001901234567[11[12[9[9[10/10/2010[Prisma SF R02";          
  $port = 3000;
             $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die($M000);
             socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 1, 'usec' => 0));
